e15ea2f79e069cd94aed732a23a9daeb5d0aea7b
Paweł Bajda <pawel.bajda@gmail.com>
2021-11-13 19:28:46 +0100

    add class JokeModel

---
+++ ChuckNorrisJoke/src/main/java/pl/bajda/pawel/ChuckNorrisJoke/model/JokeModel.java
@@ -0,0 +1,15 @@
+package pl.bajda.pawel.ChuckNorrisJoke.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JokeModel {
+
+    private String value;
+}
